1864da498992a1b6798facc0a6a5915b08cdd553
cqb13 <cqb13.main@gmail.com>
2024-09-03 21:36:55 -0400

    better scope highlights and better color preview

---
+++ lua/cqb13/plugins/mini-indentscope.lua
@@ -0,0 +1,32 @@
+return {
+	{
+		"echasnovski/mini.indentscope",
+		version = false, -- wait till new 0.7.0 release to put it back on semver
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			symbol = "┊",
+			options = { try_as_border = true },
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = {
+					"alpha",
+					"dashboard",
+					"fzf",
+					"help",
+					"lazy",
+					"lazyterm",
+					"mason",
+					"neo-tree",
+					"notify",
+					"toggleterm",
+					"Trouble",
+					"trouble",
+				},
+				callback = function()
+					vim.b.miniindentscope_disable = true
+				end,
+			})
+		end,
+	},
+}
