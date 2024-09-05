80f674da6a293a1b7117418e56a23cd5ba075ee8
cqb13 <cqb13.main@gmail.com>
2024-09-04 20:18:08 -0400

    open terminal to current keybind

--- lua/cqb13/core/keymaps.lua
+++ lua/cqb13/core/keymaps.lua
@@ -10,6 +10,12 @@ keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make split window equal size" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current window" })
 keymap.set("n", "<leader>st", "<cmd>vsp | term<CR>", { desc = "Open the terminal in a vertical split" })
+keymap.set(
+	"n",
+	"<leader>sc",
+	"<cmd>vsplit term://%:p:h//zsh<CR>",
+	{ desc = "Open the terminal to the current directory in a vertical split" }
+)
 
 -- tab keybinds
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
