aeb025f44d451da97517962ca4d11b006d6f38b5
cqb13 <cqb13.main@gmail.com>
2024-09-22 20:38:43 -0400

    clean code

--- lua/cqb13/plugins/tools/flash.lua
+++ lua/cqb13/plugins/tools/flash.lua
@@ -4,7 +4,7 @@ return {
 	opts = {},
 	keys = {
 		{
-			"q",
+			"<leader>q",
 			mode = { "n", "x", "o" },
 			function()
 				require("flash").jump()
--- lua/cqb13/plugins/ui/lualine.lua
+++ lua/cqb13/plugins/ui/lualine.lua
@@ -3,7 +3,7 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local lualine = require("lualine")
-		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+		local lazy_status = require("lazy.status")
 
 		local colors = {
 			blue = "#65D1FF",
@@ -49,7 +49,6 @@ return {
 			},
 		}
 
-		-- configure lualine with modified theme
 		lualine.setup({
 			options = {
 				theme = my_lualine_theme,
