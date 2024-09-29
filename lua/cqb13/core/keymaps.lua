e136e6d6cffc40e7f5d56a62e0c266b13ab2c0c6
cqb13 <cqb13.main@gmail.com>
2024-09-28 23:48:47 -0400

    replaced here.term with keybinds

--- lua/cqb13/core/keymaps.lua
+++ lua/cqb13/core/keymaps.lua
@@ -9,10 +9,12 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make split window equal size" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current window" })
-keymap.set("n", "<leader>st", "<cmd>vsp | term<CR>", { desc = "Open the terminal in a vertical split" })
-keymap.set(
-	"n",
-	"<leader>sc",
-	"<cmd>vsplit term://%:p:h//zsh<CR>",
-	{ desc = "Open the terminal to the current directory in a vertical split" }
-)
+
+-- terminal controls
+function OpenTerminal()
+	vim.cmd("vsplit term://zsh")
+	vim.cmd("startinsert")
+end
+
+keymap.set("n", "<C-;>", OpenTerminal, { desc = "Open the terminal to the current directory in a vertical split" })
+keymap.set("t", "<C-;>", "<cmd>close<CR>", { desc = "Close current terminal window" })
