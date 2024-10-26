15a4cae662b8af1ba1d02034e37cb4bcf8a8fa67
cqb13 <cqb13.main@gmail.com>
2024-10-26 14:22:49 -0400

    stop enter from completing

--- lua/cqb13/plugins/completions/nvim-cmp.lua
+++ lua/cqb13/plugins/completions/nvim-cmp.lua
@@ -39,7 +39,7 @@ return {
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				-- ["<CR>"] = cmp.mapping.confirm({ select = true }),
 				["<Tab>"] = cmp.mapping.confirm({ select = true }),
 			}),
 			sources = cmp.config.sources({
